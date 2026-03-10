5e68b418452b7e113ccf6c114a524669a3424b69
VadimaertsVives <vadim.aerts@student.vives.be>
2026-03-10 15:42:48 +0100

    1

---
+++ Labo17/DeEnHet/Scripts/script.js
@@ -0,0 +1,6 @@
+const setup = () => {
+    const zin = "Gisteren zat de jongen op de stoep en at de helft van de appel"
+
+}
+
+window.addEventListener("load", setup);
---
+++ Labo17/Trigram/Scripts/script.js
@@ -0,0 +1,11 @@
+const setup = () => {
+
+    let woord = "onoorbaar";
+
+    for (let i = 0; i < woord.length - 2; i++) {
+        let trigram = woord.substring(i, i + 3);
+        console.log(trigram);
+    }
+}
+
+window.addEventListener("load", setup);
