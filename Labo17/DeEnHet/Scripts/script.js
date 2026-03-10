d6f0b7a4fe6b26c64c51a4b4488cbfe1e8d07ac5
VadimaertsVives <vadim.aerts@student.vives.be>
2026-03-10 16:15:17 +0100

    67

--- Labo17/DeEnHet/Scripts/script.js
+++ Labo17/DeEnHet/Scripts/script.js
@@ -1,5 +1,26 @@
 const setup = () => {
-    const zin = "Gisteren zat de jongen op de stoep en at de helft van de appel"
+    const vervangDeMetHet = (tekst) => {
+        let resultaat = "";
+
+        for (let i = 0; i < tekst.length; i++) {
+            if (tekst[i] === 'd' && tekst[i + 1] === 'e' &&
+                (i === 0 || !isLetter(tekst[i - 1])) &&
+                (i === tekst.length - 2 || !isLetter(tekst[i + 2]))) {
+                resultaat += "het";
+                i++;
+            } else {
+                resultaat += tekst[i];
+            }
+        }
+        console.log(resultaat);
+    }
+
+    function isLetter(char) {
+        return /[a-zA-Z]/.test(char);
+    }
+
+    vervangDeMetHet("Gisteren zat de jongen op de stoep en at de helft van de appel");
+    vervangDeMetHet("de man riep de");
 
 }
 
